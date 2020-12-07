ebf6d7e8cf03871d94132e6053d4ccd507e59568
Jordan Christensen <jordan.c4922@gmail.com>
2020-12-06 23:18:36 -0700

    Initial Commit

---
+++ Snake-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snake-SwiftUI
+//
+//  Created by Jordan Christensen on 12/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Snake-SwiftUI/Snake_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snake_SwiftUIApp.swift
+//  Snake-SwiftUI
+//
+//  Created by Jordan Christensen on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Snake_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
