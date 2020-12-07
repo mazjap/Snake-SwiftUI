c68bc068ddfdbdfdfb0b0b60488507be2646dee0
Jordan Christensen <jordan.c4922@gmail.com>
2020-12-07 14:42:34 -0700

    Create Snake, Position, Direction, and board

--- Snake-SwiftUI/ContentView.swift
+++ Snake-SwiftUI/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var snek = Snake(head: .zero, body: [.zero], direction: .right)
+    
+    private var difficulty: Difficulty
+    
+    init(snake: Snake? = nil, difficulty: Difficulty = .normal) {
+        self.difficulty = difficulty
+        
+        if let snake = snake {
+            self.snek = snake
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            let size = geometry.size.width / CGFloat(difficulty.rawValue)
+            let heightCount = Int(geometry.size.height / size)
+            
+            HStack {
+                ForEach(0..<difficulty.rawValue) { i in
+                    VStack {
+                        ForEach(0..<heightCount) { j in
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(difficulty: .normal)
     }
 }
